e20a6693b292df368ea6ebec0737ab4c47e7c08e
mahon479 <mahon479@morris.umn.edu>
2024-01-25 14:56:50 -0600

    Created Todo.java files
    Co-authored-by: Ethan Graybar <grayb031@morris.umn.edu>

---
+++ server/src/main/java/umm3601/todos/Todos.java
@@ -0,0 +1,11 @@
+package umm3601.user;
+
+@SuppressWarnings({"VisibilityModifier"})
+public class Todos {
+    @SuppressWarnings({"MemberName"})
+    public String _id;
+    public String owner;
+    public int status;
+    public String body;
+    public String category;
+}
