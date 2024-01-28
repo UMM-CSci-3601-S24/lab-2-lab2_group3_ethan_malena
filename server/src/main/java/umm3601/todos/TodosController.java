59cec8ebb7ce7d72dee9ed29a9a0b7b6f20d35e2
mahon479 <mahon479@morris.umn.edu>
2024-01-27 23:33:40 -0600

    Created Malena's personal computer branch.

--- server/src/main/java/umm3601/todos/TodosController.java
+++ server/src/main/java/umm3601/todos/TodosController.java
@@ -1,4 +1,4 @@
-package umm3601.user;
+package umm3601.todos;
 
 import java.io.IOException;
 
@@ -10,52 +10,56 @@ import umm3601.Controller;
 
 public class TodosController implements Controller {
 
-    private TodosDatabase userDatabase;
-     /**
-        @param database the `Database` containing user data
-     */
-    public TodosController(TodosDatabase todosDatabase) {
-        this.todosDatabase = todosDatabase;
-    }
+  private TodosDatabase todosDatabase;
 
-        @throwsIOException
+  /**
+   * @param database the `Database` containing user data
+   */
+  public TodosController(TodosDatabase todosDatabase) {
+    this.todosDatabase = todosDatabase;
+  }
 
-    public static TodosController buildTodosController(String userDataFile) throws IOException {
-        TodosController todosController = null;
+  /**
+   * @throws IOException
+   */
+  public static TodosController buildTodosController(String todosDataFile) throws IOException {
+    TodosController todosController = null;
 
-        TodosDatabase todosDatabase = new TodosDatabase(todosDataFile);
-        todosController = new TodosController(todosDatabase);
+    TodosDatabase todosDatabase = new TodosDatabase(todosDataFile);
+    todosController = new TodosController(todosDatabase);
 
-        return todosController;
-    }
-      /**
-        @param ctx a Javalin HTTP context
-      */
-    public void getTodos(Context ctx) {
-        String id = ctx.pathParam("id");
-        Todos todos = todosDatabase.getTodos(id);
-        if (todos != null) {
-            ctx.json(todos);
-            ctx.status(HttpStatus.OK);
-        } else {
-            throw new NotFoundResponse("No todos with id " + id + " was found.");
-        }
-    }
+    return todosController;
+  }
 
-        /**
-        @param ctx a Javalin HTTP context
-      */
-    public void getTodos(Context ctx) {
-        Todo[] todos = todosDatabase.listTodos(ctx.queryParamMap());
-        ctx.json(todos);
+  /**
+   * @param ctx a Javalin HTTP context
+   */
+  public void getTodo(Context ctx) {
+    String id = ctx.pathParam("id");
+    Todos todos = todosDatabase.getTodo(id);
+    if (todos != null) {
+      ctx.json(todos);
+      ctx.status(HttpStatus.OK);
+    } else {
+      throw new NotFoundResponse("No todos with id " + id + " was found.");
     }
-      /**
-        @param server The Javalin server instance
-      */
-    @Override
-    public void addRoutes(Javalin server) {
-        server.get("/api/todos/{id}", this::getTodos);
-        server.get("/api/todos", this::getTodos);
-        server.get("/api/todos/58895985c1849992336c219b", this::getTodos);
-    }
+  }
+
+  /**
+   * @param ctx a Javalin HTTP context
+   */
+  public void getTodos(Context ctx) {
+    Todos[] todos = todosDatabase.listTodos(ctx.queryParamMap());
+    ctx.json(todos);
+  }
+
+  /**
+   * @param server The Javalin server instance
+   */
+  @Override
+  public void addRoutes(Javalin server) {
+    server.get("/api/todos/{id}", this::getTodos);
+    server.get("/api/todos", this::getTodos);
+    server.get("/api/todos/58895985c1849992336c219b", this::getTodos);
+  }
 }
